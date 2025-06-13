73ee40320c1d4eff1462d5d9b7c602a6d15bcf70
Mike Monaco <mamonaco1973@gmail.com>
2025-06-12 22:38:59 -0400

    work on ssm example

--- apply.sh
+++ apply.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export AWS_DEFAULT_REGION=us-east-2
+
 ./check_env.sh
 if [ $? -ne 0 ]; then
   echo "ERROR: Environment check failed. Exiting."
--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
 
+export AWS_DEFAULT_REGION=us-east-2
+
 cd 01-ssm
 
 terraform init

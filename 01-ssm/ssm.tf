785858da9740a7072691af7667091244e7ffc554
Mike Monaco <mamonaco1973@gmail.com>
2025-06-13 12:06:15 -0400

    work on ssm example

---
+++ 01-ssm/ssm.tf
@@ -0,0 +1,28 @@
+resource "aws_ssm_document" "install_iis_custom" {
+  name            = "InstallIISHelloWorld"
+  document_type   = "Command"
+  document_format = "JSON"
+
+  content = jsonencode({
+    schemaVersion = "2.2",
+    description   = "Install IIS and deploy a Hello World page",
+    mainSteps = [
+      {
+        action = "aws:runPowerShellScript",
+        name   = "installAndConfigureIIS",
+        inputs = {
+          runCommand = [
+            "Write-Host \"Installing IIS...\"",
+            "Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole -All -NoRestart",
+            "Start-Service W3SVC",
+            "$webRoot = \\\"C:\\\\inetpub\\\\wwwroot\\\"",
+            "$indexPath = Join-Path $webRoot \\\"index.html\\\"",
+            "$html = @\"Greetings from IIS\"@",
+            "Set-Content -Path $indexPath -Value $html -Encoding UTF8",
+            "Write-Host \"`nIIS is running. Visit: http://localhost`n\""
+          ]
+        }
+      }
+    ]
+  })
+}

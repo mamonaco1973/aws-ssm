8b8604cf61028bdfa1a46153b5a46ffd6ee9e2d4
Mike Monaco <mamonaco1973@gmail.com>
2025-06-13 13:41:00 -0400

    work on ssm example

--- apply.sh
+++ apply.sh
@@ -12,3 +12,28 @@ cd 01-ssm
 terraform init
 terraform apply -auto-approve
 cd ..
+
+/* Run SSM documents on the instances */
+sleep 60 # Wait for instances to be ready
+
+aws ssm send-command \
+  --document-name "InstallApacheOnUbuntu" \
+  --document-version "1" \
+  --targets '[{"Key":"tag:Name","Values":["ubuntu-instance"]}]' \
+  --parameters '{}' \
+  --timeout-seconds 600 \
+  --max-concurrency "50" \
+  --max-errors "0" \
+  --region us-east-2 > /dev/null
+
+aws ssm send-command \
+  --document-name "InstallIISHelloWorld" \
+  --document-version "1" \
+  --targets '[{"Key":"tag:Name","Values":["windows-instance"]}]' \
+  --parameters '{}' \
+  --timeout-seconds 600 \
+  --max-concurrency "50" \
+  --max-errors "0" \
+  --region us-east-2 > /dev/null
+
+
